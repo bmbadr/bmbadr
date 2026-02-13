e3427d2ca9b2b732b99ba91ec5a050dd669b0a5d
Bachar Bader <41931149+bmbadr@users.noreply.github.com>
2026-02-12 21:37:10 -0500

    Update disable-new-outlook.ps1

--- avd/disable-new-outlook.ps1
+++ avd/disable-new-outlook.ps1
@@ -1,4 +1,4 @@
 # Remove the New Outlook app for all users and prevent it from being re-provisioned
-Remove-AppxProvisionedPackage -AllUsers -Online -PackageName (Get-AppxPackage Microsoft.OutlookForWindows).PackageFullName -ErrorAction Ignore
+Remove-AppxProvisionedPackage -AllUsers -Online -PackageName "Microsoft.OutlookForWindows_1.2026.120.300_x64__8wekyb3d8bbwe" -ErrorAction Ignore
 # Also remove the orchestrator value that could reinstall it (documented by Microsoft)
 reg delete "HKLM\SOFTWARE\Microsoft\WindowsUpdate\Orchestrator\UScheduler_Oobe" /v OutlookUpdate /f -ErrorAction Ignore
